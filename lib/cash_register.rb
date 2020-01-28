d3027a0033cf4dd194103b4d519449fecdbdcaa6
Justin Branch <justinms@outlook.com>
2020-01-28 02:35:37 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,9 +1,14 @@
 class CashRegister
+<<<<<<< HEAD
   attr_accessor :total, :discount, :item_price
+=======
+  attr_accessor :total, :discount
+>>>>>>> b46787f4ea0aae9b42cc04bbc9769c3af08e2074
   
   def initialize(discount = 0)
     @total = 0
     @discount = discount
+<<<<<<< HEAD
     @items = []
   end
   
@@ -31,4 +36,17 @@ class CashRegister
     @total -= @item_price
   end 
   
+=======
+  end
+  
+  def add_item(item, price, quantity = 1)
+    @total += price * quantity
+  end
+  
+  def apply_discount
+    discount = (100 - @discount.to_f) / 100
+    @total = @total.to_f * discount
+    puts " "
+  end 
+>>>>>>> b46787f4ea0aae9b42cc04bbc9769c3af08e2074
 end 
